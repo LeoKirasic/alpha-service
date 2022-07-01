d0ab152dd588a0b8d73b98a5d3e479a5dc2ac8dc
Leo Kirasic <kirasicleo@gmail.com>
2022-07-01 12:53:50 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import LoginScreen from './components/LoginScreen';
+import Orders from './components/Orders';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  return <div className="">Alpha Service</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<LoginScreen />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
