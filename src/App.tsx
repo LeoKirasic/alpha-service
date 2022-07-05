e54f276900cba3668f6dea25dba02a5c2fea36c9
Leo Kirasic <kirasicleo@gmail.com>
2022-07-05 18:44:01 +0200

    Add NewOrder to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import LoginScreen from './components/LoginScreen';
 import Orders from './components/Orders';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NewOrder from './components/NewOrder';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<LoginScreen />}></Route>
-        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="orders" element={<Orders />}></Route>
+        <Route path="new-order" element={<NewOrder />}></Route>
       </Routes>
     </BrowserRouter>
   );
